1e742bd8119a67aeff05e9f1c4e7464d3659ddbd
Mudasir <thisismjeyz@gmail.com>
2026-02-28 06:35:25 +0500

    Add initial implementation for Quran and prayer time API integration

---
+++ main.py
@@ -0,0 +1,20 @@
+import requests
+import datetime
+
+date = datetime.date.today()
+
+response = requests.get("https://api.alquran.cloud/v1/surah/2")
+data = response.json()
+
+response_02 = requests.get("https://api.aladhan.com/v1/timings/{date}?latitude=31.520370&longitude=74.358749")
+print(response_02.json())
+
+for ayah in data["data"]["ayahs"]:
+    surah = ayah["text"]
+
+
+# fetch quran from APi
+# clean and understand data what's going on
+# design frontend layout it contains navebar with logo home Prayer Time Quran page link
+# in design use green color most
+# header contain navebar and header with random Quran ayah
