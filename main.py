6e0be46e345947dcddcc1a80aaaa4d56205e0a8a
Mudasir <thisismjeyz@gmail.com>
2026-03-01 12:21:21 +0500

    update header

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import datetime
 
 date = datetime.date.today()
 
-response = requests.get("https://api.alquran.cloud/v1/surah/2")
+response = requests.get("https://api.alquran.cloud/v1/surah/1")
 quranData = response.json()
 
 response_02 = requests.get("https://api.aladhan.com/v1/timings/{date}?latitude=31.520370&longitude=74.358749")
@@ -11,6 +11,7 @@ print(response_02.json())
 
 for ayah in quranData["data"]["ayahs"]:
     surah = ayah["text"]
+    print(surah)
 
 
 # clean and understand data what's going on
