d8c698a28ba89ae6bdead3a55ebf397ef97b960a
Mudasir <thisismjeyz@gmail.com>
2026-03-02 12:14:49 +0500

    Refactor API requests in main.py and remove redundant code from test.py; add functionality to fetch random Quran ayah

--- main.py
+++ main.py
@@ -1,20 +1,28 @@
 import requests
 import datetime
 from flask import Flask, request, redirect, render_template, url_for
-from test import random_ayah
+import random
 
 app  = Flask(__name__)
 app.config["SECRET_KEY"] = "123adacxcx"
 
 date = datetime.date.today()
+random_number = random.randint(1, 6237)
 
-response = requests.get("https://api.alquran.cloud/v1/surah/1/ayah/1/ar.asad")
-quranData = response.json()
+try:
+    response = requests.get("https://api.alquran.cloud/v1/surah/1/ayah/1/ar.asad")
+    response_02 = requests.get("https://api.aladhan.com/v1/timings/{date}?latitude=31.520370&longitude=74.358749")
+    response_03 = requests.get(f"https://api.alquran.cloud/v1/ayah/{random_number}/ar.asad")
+except requests.exceptions.RequestException as e:
+    print("An error occurred while making the API request:", e)
+else:
+    quranData = response.json()
+    ayah_data = response_03.json()
 
-response_02 = requests.get("https://api.aladhan.com/v1/timings/{date}?latitude=31.520370&longitude=74.358749")
+    random_ayah = ayah_data["data"]["text"]
 
-for ayah in quranData["data"]["ayahs"]:
-    surah = ayah["text"]
+    for ayah in quranData["data"]["ayahs"]:
+        surah = ayah["text"]
 
 
 @app.route("/", methods=["GET","POST"])
--- test.py
+++ test.py
@@ -1,9 +1,5 @@
 import requests
 import random
 
-random_number = random.randint(1, 6237)
 
-response = requests.get(f"https://api.alquran.cloud/v1/ayah/{random_number}/ar.asad")
-data = response.json()
-random_ayah = data["data"]["text"]
 print(random_ayah)
